df789122ecddcbcf20516b6645827b37c4b2771d
Vivek <vivek.15@mountblue.tech>
2021-06-06 01:40:54 +0530

    INitial Commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '56af296282277d6c6d586921d70650a0'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'c6753a7e-b5ad-45e5-a627-0c93ea096da7'
+};
